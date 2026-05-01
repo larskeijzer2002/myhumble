c9325ca34d5dd8b3c9fe09f358c002ef529a16a1
Lars Keijzer <info@myhumble.nl>
2026-05-01 14:04:43 +0200

    Update Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import Index from './index';
 import './index.css';
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
       <Index />
+      <Analytics />
       <SpeedInsights />
     </>
   </React.StrictMode>,
